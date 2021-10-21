c4f77a01206c688b701bb2028d58e43cc716f9b8
EdgarChangCarnero <edgar0103@hotmail.com>
2021-10-20 21:21:26 -0500

    ipad

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('.menu').on('click', function () {
+        $(this).next().slideToggle();
+    })
+});
+
+// let mostrarMenu = document.querySelector('.menu'),
+//     lista = document.querySelector('.lista');
+
+
+// mostrarMenu.addEventListener('click', () => {
+
+//     nextElementSibling.slideToggle();
+//     console.log('ddddddddddddddd');
+// });
+
+
+
+
+
+
